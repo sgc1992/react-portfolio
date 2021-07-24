176b7377a42984eb4e59e99881f71bdf7acdf6b4
Sandeep GC <sandeep_gc@hotmail.com>
2021-07-24 19:33:52 +1000

    added form component

---
+++ components/Footer/Footer.js
@@ -0,0 +1,30 @@
+
+import React from "react";
+import './footer.css';
+
+
+function Footer() {
+  return (
+
+    <div>
+
+      <div class="container2">
+
+        <div id="Contact me" class="section-title">Contact me</div>
+        <div class="section-content">0420760898</a>
+      </div>
+      <div class="section-content"><a href="mailto:">sandeep_gc@hotmail.com</a></div>
+      <div class="section-content"><a href="https://www.linkedin.com/in/sandeep-gc">Linkedin</a>
+      </div>
+      <div class="section-content"><a href="https://github.com/sgc1992/Portfolio">Github</a>
+      </div>
+
+    </div>
+
+
+
+
+  );
+}
+
+export default Footer;
