3742baa306aabb8b1194be011928d95873b8f928
Sandeep GC <sandeep_gc@hotmail.com>
2021-07-24 16:35:47 +1000

    added footer component

---
+++ components/Project/Project.js
@@ -0,0 +1,78 @@
+import React from "react";
+import './project.css';
+
+
+function Project() {
+  return (
+    <section class="section-content">
+
+    <div id="container1" class="row">
+
+      <div class="section-title">
+        <div id="Work" class="col-2">Work</div>
+      </div>
+      <div   class="col-5">
+
+        <h1 id = "password-generator">
+          <div class="section-content"><a href="https://sgc1992.github.io/Password-Generator/"> Password-Generator</a>
+          </div>
+        </h1>
+        
+      </div>
+
+      <div   class="col-6">
+
+        <h1 id = "progressive-budget">
+          <div class="section-content"><a href="https://pure-lake-47295.herokuapp.com/"> Progressive-Budget</a>
+          </div>
+        </h1>
+        
+      </div>
+      <div class="col-6">
+
+        <h1>
+          <div class="section-content"><a href="https://polar-badlands-80767.herokuapp.com/"> Tech-Blog</a>
+          </div>
+        </h1>
+      </div>
+      <div class="col-3">
+
+        <h1>
+          <div class="section-content"><a href="https://blooming-escarpment-73593.herokuapp.com/"> Note-Taker</a>
+          </div>
+        </h1>
+      </div>
+
+      
+      <div class="col-3">
+        <h3>
+          <div class="section-content"><a href="https://forester93.github.io/job-genie/"> Group-project</a>
+          </div>
+        </h3>
+      </div>
+      <div class="col-3">
+        <h4>
+          <div class="section-content"><a href="https://sgc1992.github.io/Work-Day-Scheduler/">Work-Day Scheduler</a>
+          </div>
+        </h4>
+      </div>
+      <div class="col-3">
+        <h5>
+          <div class="section-content"><a href="https://ur-fitness-tracker.herokuapp.com/?id=60e8cb7a43b31d0015962041">Fitness-Tracker</a>
+          </div>
+        </h5>
+      </div>
+      <div class="col-3">
+        <h6>
+          <div class="section-content"><a href="https://sgc1992.github.io/Weather-Dashboard/">Weather-Dashboard</a>
+          </div>
+        </h6>
+      </div>
+    </div>
+
+  </section>
+    
+  );
+}
+
+export default Project;
