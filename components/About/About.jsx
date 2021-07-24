06f143fecab426f8959ae0334e0d097b6db56613
Sandeep GC <sandeep_gc@hotmail.com>
2021-07-24 13:59:42 +1000

    created the react app

---
+++ components/About/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./about.scss"
+
+ReactDom.render(
+  <div class="section-content">
+    <div id="about me" class="row">
+      <div class="section-title">
+        <div class="col-2">About Me</div>
+      </div>
+      <div class="col-10">I am currently working as a techical customer support consultant at Ipstar which is
+        a
+        satellite broadband internet provider. I am responsible for level 1 and 2 techical support.</div>
+
+    </div>
+  </div>
+document.getElementById("root")
+);
