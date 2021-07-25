087c9d39754390b6b352439feee6c2a3f609a666
Sandeep GC <sandeep_gc@hotmail.com>
2021-07-25 15:23:52 +1000

    deploy to git hub pages

--- APP/00-practice-app/src/components/Footer/index.js
+++ APP/00-practice-app/src/components/Footer/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import './footer.css';
 
 
+
 function Footer() {
   return (
 
@@ -9,7 +10,7 @@ function Footer() {
 
       <div class="container2">
 
-        <div id="Contact me" class="section-title">Contact me</div>
+        <div id ="Contact me" class="section-title">Contact me</div>
         <div class="section-content"><a>0420760898</a>
         </div>
         <div class="section-content"><a href="mailto:">sandeep_gc@hotmail.com</a></div>
