05bd840085314494cabdb525a7c0b6c85128235f
Sandeep GC <sandeep_gc@hotmail.com>
2021-07-29 21:28:06 +1000

    added hash router

--- APP/00-practice-app/src/index.js
+++ APP/00-practice-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,23 +11,24 @@ import Footer from './components/Footer';
 import Form from './components/Form';
 
 ReactDOM.render(
-  
-    <Router>
-      <Route exact path="/react-portfolio">
-        <Nav />
-        <About />
-        <Project />
-        <Footer />
-      </Route>
-      <Route exact path="/contact">
-        <Form />
-      </Route>
-    </Router>
-  ,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <HashRouter basename={process.env.PUBLIC_URL}>
+    <Switch>
+      <Route exact path="/">
+        <Nav/>
+          <About/>
+            <Project/>
+              <Footer/>
+      </Route>
+              <Route exact path="/contact" component={Form} />
+  </Switch>
+
+  </HashRouter>
+          ,
+          document.getElementById('root')
+          );
+
+          // If you want to start measuring performance in your app, pass a function
+          // to log results (for example: reportWebVitals(console.log))
+          // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+          reportWebVitals();
